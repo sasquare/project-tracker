a03d7160607624fc38fa7fdfec68731981a0ea9b
sasquare <femisasa@gmail.com>
2025-04-02 07:52:54 +0100

    Increase password column length to 255

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from flask_login import UserMixin
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), default='user')  # 'user' or 'admin'
 
 class Project(db.Model):
